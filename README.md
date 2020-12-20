4c03c6dad5c42194d5da0e3f01a962eb23f1a87c
Bryan Lilburn <bryanlilburn@gmail.com>
2020-12-19 19:38:42 -0800

    Updated README, added jpg for README, added db.js, added manifest.webmanifest, and added server-worker.js

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # budget-tracker
+
+## Description 
+
+This is a budget tracker application that is designed to provide the user with the ability to log purchases. A user will be able to track the withdrawals and deposits with or without an internet connection. A graph located at the bottom of the application provides a visual representation of withdrawals and deposits made.
+
+## Table of Contents 
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [Questions](#questions)
+
+## Installation
+
+Required installation for this application is as follows:
+*Access to internet on the desired device.
+*A web broswer on the desired device.
+
+
+## Usage 
+
+Usage is as follows:
+*In the "Name of transaction" field the user can type a description of the transaction. In the "Transaction amount" field the user can type the desired amount of the transaction. The user can then click the "+Add Funds" button if the transaction will increase the budget or click the "-Subtract Funds" button if the transaction will decrease the budget.
+
+![empoy](./public\assets\budget.JPG)
+
+
+## Questions
+
+GitHub username: blilburn
---
+++ public/service-worker.js
@@ -0,0 +1,65 @@
+
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
+
+const iconSizes = ["192", "512"];
+const iconFiles = iconSizes.map(
+  (size) => `/public/icons/icon-${size}x${size}.png`
+);
+
+const FILES_TO_PRECACHE = [
+  "/",
+  "/index.js",
+  "/style.css",
+  "/favicon.ico",
+  "/manifest.webmanifest",
+].concat(iconFiles);
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches
+      .open(PRECACHE)
+      .then((cache) => cache.addAll(FILES_TO_PRECACHE))
+      .then(self.skipWaiting())
+  );
+});
+
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener('activate', (event) => {
+  const currentCaches = [PRECACHE, RUNTIME];
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
+      })
+      .then((cachesToDelete) => {
+        return Promise.all(
+          cachesToDelete.map((cacheToDelete) => {
+            return caches.delete(cacheToDelete);
+          })
+        );
+      })
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then((cachedResponse) => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return caches.open(RUNTIME).then((cache) => {
+          return fetch(event.request).then((response) => {
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    );
+  }
+});
